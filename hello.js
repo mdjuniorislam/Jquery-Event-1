592cb62b0c4c3b845dc783a11ea8ed064d3ec8fc
mdjuniorislam <70217801+mdjuniorislam@users.noreply.github.com>
2020-09-20 04:10:19 +0600

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $(".click button").click(function(){
+        $('.click h2').hide(2000);
+    });
+
+});
+
+$(document).ready(function(){
+    $(".submit button").click(function(){
+        $('.submit p').css("background-color", "#000")
+    });
+
+});
+
+$(document).ready(function(){
+    $(".text-color button").click(function(){
+        $('.text-color h2').css({"background-color":"#45143D" ,
+        "color":"#fff"
+    
+    
+    });
+    });
+
+});
